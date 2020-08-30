045aec1a40ad72e0a6736544cbb57f639f292f7d
Zineeddine998 <55639522+Zineeddine998@users.noreply.github.com>
2020-08-30 02:05:56 +0100

    added validation

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -41,27 +41,126 @@ __decorate([
 UsernamePasswordInput = __decorate([
     type_graphql_1.InputType()
 ], UsernamePasswordInput);
+let FieldError = class FieldError {
+};
+__decorate([
+    type_graphql_1.Field(),
+    __metadata("design:type", String)
+], FieldError.prototype, "field", void 0);
+__decorate([
+    type_graphql_1.Field(),
+    __metadata("design:type", String)
+], FieldError.prototype, "message", void 0);
+FieldError = __decorate([
+    type_graphql_1.ObjectType()
+], FieldError);
+let UserResponse = class UserResponse {
+};
+__decorate([
+    type_graphql_1.Field(() => [FieldError], { nullable: true }),
+    __metadata("design:type", Array)
+], UserResponse.prototype, "errors", void 0);
+__decorate([
+    type_graphql_1.Field(() => User_1.User, { nullable: true }),
+    __metadata("design:type", User_1.User)
+], UserResponse.prototype, "user", void 0);
+UserResponse = __decorate([
+    type_graphql_1.ObjectType()
+], UserResponse);
 let UserResolver = class UserResolver {
     register(options, { em }) {
         return __awaiter(this, void 0, void 0, function* () {
+            if (options.username.length <= 2) {
+                return {
+                    errors: [
+                        {
+                            field: 'username',
+                            message: 'length of the username must be longer than 2'
+                        }
+                    ]
+                };
+            }
+            if (options.password.length <= 2) {
+                return {
+                    errors: [
+                        {
+                            field: 'username',
+                            message: 'length of the password must be longer than 2'
+                        }
+                    ]
+                };
+            }
             const hashedPassword = yield argon2_1.default.hash(options.password);
             const user = em.create(User_1.User, {
                 username: options.username,
                 password: hashedPassword
             });
-            yield em.persistAndFlush(user);
-            return user;
+            try {
+                yield em.persistAndFlush(user);
+            }
+            catch (err) {
+                if (err.code === '23505') {
+                    return {
+                        errors: [
+                            {
+                                field: 'username',
+                                message: 'the username already exist'
+                            }
+                        ]
+                    };
+                }
+            }
+            return {
+                user,
+            };
+        });
+    }
+    login(options, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const user = yield em.findOne(User_1.User, { username: options.username });
+            if (!user) {
+                return {
+                    errors: [
+                        {
+                            field: 'username',
+                            message: 'the user do not exist'
+                        },
+                    ],
+                };
+            }
+            const validator = yield argon2_1.default.verify(user.password, options.password);
+            if (!validator) {
+                return {
+                    errors: [
+                        {
+                            field: 'password',
+                            message: 'incorrect password'
+                        }
+                    ]
+                };
+            }
+            return {
+                user,
+            };
         });
     }
 };
 __decorate([
-    type_graphql_1.Mutation(() => User_1.User),
+    type_graphql_1.Mutation(() => UserResponse),
     __param(0, type_graphql_1.Arg("options", () => UsernamePasswordInput)),
     __param(1, type_graphql_1.Ctx()),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
     __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "register", null);
+__decorate([
+    type_graphql_1.Mutation(() => UserResponse),
+    __param(0, type_graphql_1.Arg("options", () => UsernamePasswordInput)),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
+    __metadata("design:returntype", Promise)
+], UserResolver.prototype, "login", null);
 UserResolver = __decorate([
     type_graphql_1.Resolver()
 ], UserResolver);
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import {Resolver, Mutation, InputType, Field, Arg, Ctx} from 'type-graphql';
+import {Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType} from 'type-graphql';
 import {MyContext} from "../types";
 import argon2 from 'argon2';
 import {User} from "../entities/User";
@@ -11,19 +11,117 @@ class UsernamePasswordInput {
     password : string;
 }
 
+@ObjectType()
+class FieldError {
+    @Field()
+    field : string;
+
+    @Field()
+    message : string;
+
+}
+
+@ObjectType()
+class  UserResponse {
+    @Field(() => [FieldError], {nullable : true})
+    errors? : FieldError[];
+
+    @Field(() => User , { nullable : true})
+    user? : User;
+}
+
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg("options", () => UsernamePasswordInput) options : UsernamePasswordInput,
         @Ctx() {em} : MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if(options.username.length  <= 2) {
+            return {
+                errors: [
+                    {
+                        field : 'username',
+                        message : 'length of the username must be longer than 2'
+                    }
+                ]
+            }
+        }
+        if(options.password.length  <= 2) {
+            return {
+                errors: [
+                    {
+                        field : 'username',
+                        message : 'length of the password must be longer than 2'
+                    }
+                ]
+            }
+        }
         const hashedPassword = await argon2.hash(options.password);
        const user =  em.create(User, {
            username : options.username,
            password : hashedPassword
        });
-       await em.persistAndFlush(user);
-       return user;
+       try {
+           await em.persistAndFlush(user);
+       }catch (err) {
+           if(err.code === '23505'){
+               return {
+                   errors : [
+                       {
+                           field :  'username',
+                           message : 'the username already exist'
+                       }
+                   ]
+               }
+           }
+           //console.log('message : ' + err.message);
+       }
+
+       return {
+           user,
+       }
+    }
+
+
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg("options", () => UsernamePasswordInput) options : UsernamePasswordInput,
+        @Ctx() {em} : MyContext
+    )  : Promise<UserResponse>{
+        const user = await em.findOne(User, {username: options.username});
+
+        if(!user){
+            return {
+                errors : [
+                    {
+                        field : 'username',
+                        message  : 'the user do not exist'
+                    },
+                ],
+            };
+
+        }
+
+
+        const validator = await argon2.verify(user.password, options.password);
+
+        if(!validator){
+            return {
+                errors : [
+                    {
+                        field : 'password',
+                        message : 'incorrect password'
+                    }
+                ]
+            }
+        }
+
+        return {
+            user,
+        }
+
+
     }
 }
